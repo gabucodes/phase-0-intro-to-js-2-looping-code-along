773f1a0a3b1526b7c90d5f57495f7a2ee8a02ce3
Gabriel <kipkoechgabriel27@gmail.com>
2025-03-01 22:28:39 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+// helpers.js (Assuming this file is required in your test)
+
+// Function that creates an array of thank you messages
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function that counts down from a given number to zero and logs each number
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Export functions for testing
+  module.exports = {
+    writeCards,
+    countDown
+  };
+  
